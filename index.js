c1324203a13de9cf9ecc5cf195d94fba8c790d27
LaRaye Johnson <lnj2105@gmail.com>
2019-08-26 19:59:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-
+const callback = function (el, i, arr) {
+  console.log('The current element is', el);
+  console.log('The index of the current element is', i);
+  console.log('The current array is', arr);
+  console.log('----------------');
+};
 
 function logDriverNames(driversArr) {
   
